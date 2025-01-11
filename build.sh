70ad33fe6aface4fba33691cca20e365b6c6fcde
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-01-11 23:49:59 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 printf "\033c\033[43;30m"
-nasm mysys.s -o mysys.bin
-python3 linux.py
+ls *.c
+printf "give me a .c file gcc? "
+read aaa
+cp app.data cd1.iso
+nasm mysys.s -o /tmp/mysys.o
+/usr/bin/x86_64-linux-gnu-as -o /tmp/boot.o ./boot.s
+/usr/bin/x86_64-linux-gnu-gcc -c -m64 -nostdlib $aaa -o /tmp/kernel.o
+/usr/bin/x86_64-linux-gnu-ld -nostdlib -T ./link.ld /tmp/boot.o /tmp/kernel.o  -o /tmp/kernel.bin 
+/usr/bin/x86_64-linux-gnu-objcopy /tmp/kernel.bin /tmp/hello.c64  --output-target binary 
+cat /tmp/mysys.o /tmp/hello.c64 > /tmp/sys.bin
+dd if=/tmp/sys.bin of=cd1.iso bs=1k seek=58 conv=notrunc
 qemu-system-x86_64 -cdrom cd1.iso
--- mysys.s
+++ mysys.s
@@ -1,127 +1,117 @@
-; hello.asm
-[BITS 16]          ; Configuração para 16 bits (modo real)
-[ORG 0x7C00]       ; Define o endereço de carregamento
+[BITS 16]
+[ORG 0x7C00]
+jmp SwitchToLongMode     
+%define PAGE_PRESENT    (1 << 0)
+%define PAGE_WRITE      (1 << 1)
 
-start:
-    jmp cons
-msg db "my system version 1.1 put you message here....", 0 ; Mensagem a ser impressa (terminada em 0)
+%define CODE_SEG     0x0008
+%define DATA_SEG     0x0010
 
-times 510-($-$$) db 0 ; Preenche até 510 bytes
-dw 0xAA55             ; Assinatura do setor de boot (0xAA55)
-cons:
-    push ds
-    mov ax,0xb800
-    mov ds,ax
-    mov cx,4000
-    mov ax,0x6020
-    mov bx,0
-loop1:
-    ds
-    mov [bx],ax
-    inc bx
-    inc bx
-    dec cx
-    cmp cx,0
-    jnz loop1
+ALIGN 4
+IDT:
+    .Length       dw 0
+    .Base         dd 0
 
-    pop ds
 
-jmp _main
-nop
-oem             db      'MY OEM  '
-bsector         dw      200h
-scluster        db      1h 
-rsector         dw      1h
-tfat            db      2h
-rent            dw      0e0h
-tsectors        dw      0b40h
-media           db      0f0h 
-sfat            dw      9h
-strak           dw      12h
-head            dw      2h
-hidden          dd      0h
-large           dd      0h
-drive           db      0h
-flag            db      0h
-sig             db      29h
-vol             dd      0ffffffffh
-label           db      'MY LABEL    '
-id              db      'FAT12   '
-;--------------------------------------------------------
-eess            dw      0
-ees1            dw      0
-_main:
-mov dx,0xe0
- 
-main2:
-    clc
-    mov si,area0
-    mov ah,0x42
+SwitchToLongMode:
+    push di                           
+    mov ecx, 0x1000
+    xor eax, eax
+    cld
+    rep stosd
+    pop di                          
+
+
+    lea eax, [es:di + 0x1000]         
+    or eax, PAGE_PRESENT | PAGE_WRITE 
+    mov [es:di], eax                  
+
+
+    lea eax, [es:di + 0x2000]         
+    or eax, PAGE_PRESENT | PAGE_WRITE 
+    mov [es:di + 0x1000], eax         
+
     
-;int load sectores into memory
-    int 0x13
-    jnc printss
-    jmp error
-   
+
+    lea eax, [es:di + 0x3000]         
+    or eax, PAGE_PRESENT | PAGE_WRITE 
+    mov [es:di + 0x2000], eax         
+    
+    
+    push di                           
+    lea di, [di + 0x3000]             
+    mov eax, PAGE_PRESENT | PAGE_WRITE    
 
 
-.hangss:
-    cli             ; Desabilita interrupções
-    hlt             ; Entra em estado de espera
+
+.LoopPageTable:
+    mov [es:di], eax
+    add eax, 0x1000
+    add di, 8
+    cmp eax, 0x200000
+    jb .LoopPageTable
+
+    pop di           
+    
+    ; Disable IRQs
+    mov al, 0xFF     
+    out 0xA1, al
+    out 0x21, al
+    
+    nop
+    nop
+
+    lidt [IDT]       
+
+    ; Enter long mode.
+    mov eax, 10100000b
+    mov cr4, eax
+      
+    mov edx, edi      
+    mov cr3, edx
+      
+    mov ecx, 0xC0000080
+    rdmsr    
+
+    or eax, 0x00000100 
+    wrmsr
+      
+    mov ebx, cr0       
+    or ebx,0x80000001  
+    mov cr0, ebx                    
+
+    lgdt [GDT.Pointer] 
+      
+    jmp CODE_SEG:LongMode
+      
+      
+    ; Global Descriptor Table
+GDT:
+.Null:
+    dq 0x0000000000000000
+
+.Code:
+    dq 0x00209A0000000000
+    dq 0x0000920000000000
+      
+ALIGN 4
+    dw 0                 
+
+.Pointer:
+    dw $ - GDT - 1       
+    dd GDT               
 
 
-printss:
-    mov ax,0x1000
-    mov es,ax
-    mov ax, 32768     ; Ponteiro para a mensagem
-    add ax,0
-    mov bp,ax
-prints:
-    push bp
-    mov bl,0x60
-    mov bh,0
-    mov dh,0
-    mov dl,0
-    mov cx,1024
-    mov ah,0x13
-    mov al,0x1
-    int 0x10
-    mov ax,0
-    int 0x16
-    pop ax
-    add ax,1024
-    mov bp,ax
-    jmp prints
-.hang:
-    cli             ; Desabilita interrupções
-    hlt             ; Entra em estado de espera
-error:
-    mov bp,err
-    mov bl,0x60
-    mov bh,0
-    mov dh,0
-    mov dl,0
-    mov cx,32
-    mov ah,0x13
-    mov al,0x1
-    int 0x10
-    mov ax,0
-    int 0x16
-.hangs:
-    cli             ; Desabilita interrupções
-    hlt             ; Entra em estado de espera
-hexs:
-db "0123456789abcdf"
-err:
-db "error......................................................................................."
-area0:
-db 0x10,0x0
-dw 32
-dw 0x0,0x1000
-dw 0
-dw 0
-dw 0
-times 32 db 0 ; Preenche até 510 bytes
-area2:
-area1:
-times 448000-($-$$) db 0 ; Preenche até 510 bytes
+[BITS 64]      
+LongMode:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax    
+    jmp coms  
+
+times 510-($-$$) db 0 ; Preencher até 510 bytes
+dw 0xAA55             ; Assinatura do setor de boot
+coms:

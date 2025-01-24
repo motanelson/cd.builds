1a6486fb243fc5cd346389fe79558e54e3a8c552
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-01-24 15:26:42 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/bash
 aaa=$1
-cp app.data ./tmp/$aaa.iso
 /usr/bin/x86_64-linux-gnu-gcc -c -m64 -nostdlib ./uploads/$aaa.c -o /tmp/$aaa.o
-/usr/bin/x86_64-linux-gnu-ld -nostdlib -T ./link.ld /tmp/boot.o /tmp/$aaa.o  -o /tmp/a$aaa.bin 
-/usr/bin/x86_64-linux-gnu-objcopy /tmp/a$aaa.bin /tmp/$aaa.c32  --output-target binary 
-cat /tmp/mysys.o /tmp/$aaa.c32 > /tmp/$aaa.bin
-dd if=/tmp/$aaa.bin of=./tmp/$aaa.iso bs=1k seek=58 conv=notrunc
-rm /tmp/$aaa.o
-rm /tmp/a$aaa.bin
-rm /tmp/$aaa.c32
-rm /tmp/$aaa.bin
+
